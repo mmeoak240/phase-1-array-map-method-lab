0c48c7ecfd7d1dc84179615e56f4e10d6f639383
Michael Meoak <mmeoak240@gmail.com>
2022-03-25 18:44:26 -0700

    commit tree fcaf952a0140bcba7dfcc167eaca4fbdf33f810b

--- index.js
+++ index.js
@@ -11,6 +11,71 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {let tutorial = tutorials.map(function(phrase) {
+//   let words = phrase.split(" ")
+//  words.map(word => {
+//    word[0].toUpperCase()
+//    return words.join(' ')
+//   })
+// })
+
+// function titleCased() {
+//    tutorials.map(function(word) {
+//   let words = word.split(" "); 
+//   return words.map(function(word){
+//   return word.charAt(0).toUpperCase()+ word.substring(1)
+//   }).join(" "); 
+
+// });
+
+
+
+
+// const titleCased = () => {
+//   tutorials.map(function(phrase) {
+//       let words = phrase.split(" ")
+//       words.map(function(word) {
+//         let correction = word.charAt(0)
+//         return correction.toUpperCase()
+//       })
+//     })
+// }
+
+
+// const titleCased = tutorials.map(tutorial => {
+//   return tutorial.split(' ')
+//   .map((word) => word[0].toUpperCase() + word.slice(1))  /// this one supposedly worked but unsure how the methods aren't attached to anything.
+//   .join(' ')
+// })
+
+
+
+
+//  function titleCased() {
+//    tutorials.map(function(phrase) {
+//   let words = phrase.split(" ")
+//   words.map(word => {
+//     let correction = word.charAt(0)
+//     correction.toUpperCase()
+//   })
+//   return correction.join(' ')
+// })
+
 const titleCased = () => {
-  return tutorials
-}
+return tutorials.map(tutorial => {
+  const splitTitle = tutorial.split(" ");    const upperCaseTitle = splitTitle.map(function (word) {
+   word.charAt(0).toUpperCase() + word.slice(1)  
+  return (`${word.charAt(0).toUpperCase()}${word.slice(1)}`)    })    
+  return upperCaseTitle.join(" ");  })}
+
+
+
+// .startsWith()
+// .charAt(i)
+
+
+
+// const titleCased = () => {
+  
+//   return tutorials
+// }
